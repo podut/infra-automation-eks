5c77e238b03d8bee3f5718a7862339731a878e59
Nana Janashia <nana@macbook.local>
2024-02-04 10:48:20 +0100

    enable eks addons

--- main-eks.tf
+++ main-eks.tf
@@ -65,3 +65,17 @@ module "eks" {
 
   tags = var.tags
 }
+
+module "eks_blueprints_addons" {
+  source = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.0" #ensure to update this to the latest/desired version
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_aws_load_balancer_controller    = true
+  enable_metrics_server                  = true
+  enable_cluster_autoscaler              = true
+}
